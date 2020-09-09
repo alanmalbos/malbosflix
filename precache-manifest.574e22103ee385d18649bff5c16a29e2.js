1b837451a057edf1c40ec49a9768121089ec0eb2
Alan Malbos <alanmalbos@gmail.com>
2020-09-09 15:00:39 -0300

    Updates

---
+++ precache-manifest.574e22103ee385d18649bff5c16a29e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63ebfd49a897a187e181787b1b2f73ee",
+    "url": "/malbosflix/index.html"
+  },
+  {
+    "revision": "dfcce02d28725c912909",
+    "url": "/malbosflix/static/css/main.cac9c799.chunk.css"
+  },
+  {
+    "revision": "9739300b7d44f919cdc7",
+    "url": "/malbosflix/static/js/2.ca913881.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/malbosflix/static/js/2.ca913881.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfcce02d28725c912909",
+    "url": "/malbosflix/static/js/main.caf81ff8.chunk.js"
+  },
+  {
+    "revision": "8e44b402aaa38e91c117",
+    "url": "/malbosflix/static/js/runtime-main.920eb60b.js"
+  }
+]);
